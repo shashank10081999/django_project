3bd64b22c02f510239e23de3a3a5ed5d006dd964
Shashank Garimella <shashank25225@gmail.com>
2024-02-13 00:59:34 -0600

    Simple django project completed

---
+++ my_site/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("" , views.start_page , name = "starting-page"),
+    path("posts" , views.posts , name="all-posts"),
+    path("posts/<slug:slug>" , views.post_detail , name = "post-detail-page"),
+]
